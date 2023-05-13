b47712b73eb12c387d56f8cc6ae7b076f8ba6464
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2023-05-14 01:31:14 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,26 @@
+import Image from 'next/image'
+
+
 import styles from './page.module.css'
 
-export default function Home({ movies }) {
-  if (!movies) {
-    return <div>Loading...</div>
-  }
 
+
+
+export default async function Home() {
+  
+  const data =await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}`)
+  const res =await data.json()
+  console.log(res)
   return (
     <main>
       <h1>Hello next 13</h1>
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-        </div>
+      {res.results.map((movie) => (
+        <div>
+        <h2>{movie.title}</h2></div>
+
       ))}
+     
+     
     </main>
   )
-}
-
-export async function getStaticProps() {
-  const res = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}`)
-  const data = await res.json()
-  const movies = data.results
-  return { props: { movies } }
 }
